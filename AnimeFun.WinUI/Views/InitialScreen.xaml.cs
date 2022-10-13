81a73f58ff29f4d2de155b9c86ab9098555968f7
熙 王 <1324771795@qq.com>
2022-10-13 23:42:39 +0800

    添加首屏

---
+++ AnimeFun.WinUI/Views/InitialScreen.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Animation;
+using System;
+using System.Threading.Tasks;
+
+namespace AnimeFun.WinUI.Views
+{
+    public sealed partial class InitialScreen : Page
+    {
+        public InitialScreen()
+        {
+            InitializeComponent();
+        }
+
+        private async void PageMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Task.Delay(1500);
+            imgLogoGif.Visibility = Visibility.Collapsed;
+            Storyboard storyboard = new Storyboard();
+            DoubleAnimation doubleAnimation = new()
+            {
+                To = -60,
+                Duration = new TimeSpan(0, 0, 0, 0, 500)
+            };
+            Storyboard.SetTargetProperty(doubleAnimation, "Y");
+            Storyboard.SetTarget(doubleAnimation, trnMain);
+            storyboard.Children.Add(doubleAnimation);
+            storyboard.Begin();
+            storyboard.Completed += delegate
+            {
+                proRing.Visibility = Visibility.Visible;
+            };
+        }
+    }
+}
