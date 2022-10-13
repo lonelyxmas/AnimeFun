ed8737a55f34af81eb3a1b6a964844ca8c911fa0
熙 王 <1324771795@qq.com>
2022-10-13 23:53:56 +0800

    wx

--- AnimeFun.WinUI/Views/InitialScreen.xaml.cs
+++ AnimeFun.WinUI/Views/InitialScreen.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Animation;
-using System;
-using System.Threading.Tasks;
 
 namespace AnimeFun.WinUI.Views
 {
