7691c1265c2fffd296302294e23780fcc7550d55
熙 王 <1324771795@qq.com>
2022-11-23 14:19:23 +0800

    删除表结构

--- AnimeFun.Service/DataFactory/AnimeFunContext.cs
+++ AnimeFun.Service/DataFactory/AnimeFunContext.cs
@@ -1,31 +1,10 @@
 ﻿using AnimeFun.Core.Models.AnimeFun;
-using AnimeFun.Service.DataFactory.Configurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace AnimeFun.Service.DataFactory
 {
     public class AnimeFunContext : DbContext
     {
-        /// <summary>
-        /// 视频信息
-        /// </summary>
-        public DbSet<VideoInfo> VideoInfos { get; set; }
-
-        /// <summary>
-        /// 剧集
-        /// </summary>
-        public DbSet<Episode> Episodes { get; set; }
-
-        /// <summary>
-        /// 视频标签
-        /// </summary>
-        public DbSet<VideoTag> VideoTags { get; set; }
-
-        /// <summary>
-        /// 推荐视频
-        /// </summary>
-        public DbSet<RecommendVideo> RecommendVideos { get; set; }
-
         public AnimeFunContext()
         {
             ChangeTracker.StateChanged += (sender, args) =>
@@ -48,10 +27,7 @@ namespace AnimeFun.Service.DataFactory
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfiguration(new VideoInfoConfiguration());
-            modelBuilder.ApplyConfiguration(new EpisodeConfiguration());
-            modelBuilder.ApplyConfiguration(new VideoTagConfiguration());
-            modelBuilder.ApplyConfiguration(new RecommendVideoConfiguration());
+
         }
     }
 }
--- AnimeFun.Service/DataFactory/InitialAnimeFun.cs
+++ AnimeFun.Service/DataFactory/InitialAnimeFun.cs
@@ -1,6 +1,4 @@
-﻿using AnimeFun.Core.Models.AnimeFun;
-
-namespace AnimeFun.Service.DataFactory
+﻿namespace AnimeFun.Service.DataFactory
 {
     public class InitialAnimeFun
     {
@@ -9,73 +7,7 @@ namespace AnimeFun.Service.DataFactory
         /// </summary>
         public static void InitialData()
         {
-            AnimeFunContext context = new();
 
-            bool state = context.VideoInfos.Any()
-                         && context.Episodes.Any()
-                         && context.VideoTags.Any()
-                         && context.RecommendVideos.Any();
-            if (!state)
-            {
-                List<VideoTag> videoTags = GetVideoTags();
-                List<VideoInfo> videoInfos = GetVideoInfos(videoTags);
-                context.VideoTags.AddRange(videoTags);
-                context.VideoInfos.AddRange(videoInfos);
-
-                context.SaveChanges();
-            }
-        }
-
-        /// <summary>
-        /// 视频标签
-        /// </summary>
-        /// <returns></returns>
-        private static List<VideoTag> GetVideoTags()
-        {
-            List<VideoTag> videoTags = new();
-            for (int i = 0; i < 30; i++)
-            {
-                videoTags.Add(new VideoTag
-                {
-                    Name = $"测试视频标签 {i + 1}",
-                    Description = $"描述文字。。。"
-                });
-            }
-
-            return videoTags;
-        }
-
-        /// <summary>
-        /// 视频信息
-        /// </summary>
-        /// <param name="videoTags"></param>
-        /// <returns></returns>
-        private static List<VideoInfo> GetVideoInfos(List<VideoTag> videoTags)
-        {
-            List<VideoInfo> videoInfos = new();
-            for (int i = 0; i < videoTags.Count; i++)
-            {
-                VideoInfo videoInfo = new()
-                {
-                    Name = $"测试视频信息 {i + 1}",
-                    Description = $"描述文字。。。",
-                    Cover = "Test.png"
-                };
-                videoInfo.VideoTags.Add(videoTags[i]);
-                for (int j = 0; j < 24; j++)
-                {
-                    videoInfo.Episodes.Add(new Episode
-                    {
-                        Title = $"测试剧集 {i + 1}",
-                        Cover = "Test.png",
-                        SourceId = "AAAA-BBBB-CCCC-DDDD"
-                    });
-                }
-
-                videoInfos.Add(videoInfo);
-            }
-
-            return videoInfos;
         }
     }
 }
